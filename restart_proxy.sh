5f59b669187ad9ff564feb839387e713aec8d03a
Pavel Samokhin <144112238+st117168@users.noreply.github.com>
2026-08-19 00:03:03 +0500

    Add files via upload

--- restart_proxy.sh
+++ restart_proxy.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
 
-~/.shortcuts/tg-ws-proxy-android-termux/close_proxy.sh
-~/.shortcuts/tg-ws-proxy-android-termux/start_proxy.sh
+PROXY_DIR="$HOME/tg-ws-proxy/proxy"
+PROXY_SERVER="127.0.0.1"
+PROXY_PORT="1443"
+
+PID=$(pgrep -f "tg_ws_proxy.py")
+
+if [ -n "$PID" ]; then
+    kill "$PID"
+    termux-wake-unlock
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - proxy stopped." >> ~/tg-ws-proxy/proxy_log.txt
+else
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - proxy already stopped." >> ~/tg-ws-proxy/proxy_log.txt
+fi
+
+extract_secret() {
+    echo "$1" | grep -oP "Secret:\s+\K[a-f0-9]+" | head -1
+}
+
+cd "$PROXY_DIR" || { echo "$(date +"%Y-%m-%d %H:%M:%S") - error PROXY_DIR" >> ~/tg-ws-proxy/proxy_log.txt; exit 1; }
+
+TEMP_LOG=$(mktemp)
+nohup python tg_ws_proxy.py > "$TEMP_LOG" 2>&1 &
+echo "$(date +"%Y-%m-%d %H:%M:%S") - proxy started." >> ~/tg-ws-proxy/proxy_log.txt
+
+SECRET=$(extract_secret "$(cat $TEMP_LOG)")
+rm -f "$TEMP_LOG"
+
+if [ -n "$SECRET" ]; then
+    termux-open "tg://proxy?server=$PROXY_SERVER&port=$PROXY_PORT&secret=dd$SECRET"
+        echo "$(date +"%Y-%m-%d %H:%M:%S") - telegram opened." >> ~/tg-ws-proxy/proxy_log.txt
+    termux-wake-lock
+fi
